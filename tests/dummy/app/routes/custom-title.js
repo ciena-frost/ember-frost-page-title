1bfe8b1add7da3588376430ec56b5d78688086a5
John Fellman <jfellman@ciena.com>
2018-03-26 15:36:00 -0700

    improve dummy examples

--- tests/dummy/app/routes/custom-title.js
+++ tests/dummy/app/routes/custom-title.js
@@ -1,6 +1,10 @@
 import Ember from 'ember'
+import FrostPageTitleMixin from 'ember-frost-page-title/mixins/frost-page-title'
 
 const {Route} = Ember
 
-export default Route.extend({
+export default Route.extend(FrostPageTitleMixin, {
+  frostPageTitle (sections, defaultTitle) {
+    return ['Hello', 'Custom', 'Title']
+  }
 })
--- tests/dummy/app/routes/custom-title/nested.js
+++ tests/dummy/app/routes/custom-title/nested.js
@@ -1,6 +1,15 @@
 import Ember from 'ember'
+import FrostPageTitleMixin from 'ember-frost-page-title/mixins/frost-page-title'
 
 const {Route} = Ember
 
-export default Route.extend({
+export default Route.extend(FrostPageTitleMixin, {
+  frostPageTitle (sections, defaultTitle) {
+    return sections.map(section => {
+      if (section === 'Title') {
+        return `(Nested) ${section}`
+      }
+      return section
+    })
+  }
 })
