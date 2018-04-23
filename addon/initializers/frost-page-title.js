91e4bc8d590f3d030bed4fdbc6d627d6da1a1fd3
John Fellman <jfellman@ciena.com>
2018-04-23 11:34:19 -0700

    fix initialization so this package does nothing if not used

--- addon/initializers/frost-page-title.js
+++ addon/initializers/frost-page-title.js
@@ -2,7 +2,14 @@
  * @overview initializes router with frost-page-title service
  */
 
+import config from 'ember-get-config'
+
+const {APP} = config
+
 export function initialize (application) {
+  if (!APP['frost-page-title'] || !APP['frost-page-title'].defaultTitle) {
+    return
+  }
   application.inject('router', 'frost-page-title', 'service:frost-page-title')
 }
 
